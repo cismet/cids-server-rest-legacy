6b0fc9fb388248716a3841c2f925dbea18783ff8
Pascal Dihé <pascal@cismet.de>
2016-03-04 18:15:49 +0100

    #31 tests updated

---
+++ src/test/java/de/cismet/cidsx/client/connector/test/RESTfulInterfaceTest.java
@@ -0,0 +1,199 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cidsx.client.connector.test;
+
+
+import Sirius.server.middleware.types.MetaObject;
+import Sirius.server.newuser.User;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.github.fge.jackson.JsonLoader;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.Base64;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+import de.cismet.cids.dynamics.CidsBean;
+import de.cismet.cids.dynamics.CidsBeanInfo;
+import de.cismet.cidsx.client.connector.RESTfulInterfaceConnector;
+import static de.cismet.cidsx.client.connector.RESTfulInterfaceConnector.ENTITIES_API;
+import java.io.IOException;
+import java.rmi.RemoteException;
+import java.util.Properties;
+import java.util.PropertyResourceBundle;
+import javax.ws.rs.core.MultivaluedMap;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pascal Dihé <pascal.dihe@cismet.de>
+ */
+public class RESTfulInterfaceTest extends RESTfulInterfaceConnector {
+    
+    private static String HOST;
+    private static String BASIC_AUTH_STRING;
+    private final CidsBean defaultCidsBean;
+    private static RESTfulInterfaceTest INSTANCE;
+    
+    private final static Logger LOGGER = Logger.getLogger(RESTfulInterfaceTest.class);
+    
+    
+    public RESTfulInterfaceTest() throws IOException 
+    {
+        super(HOST);
+        
+        final JsonNode node = JsonLoader.fromURL(RESTfulInterfaceConnector.class.getResource("metadata.json"));
+        this.defaultCidsBean = insertDefaultCidsBean(node);
+                
+                if(INSTANCE == null) {
+                    INSTANCE = this;
+                }
+    }
+    
+     /**
+     * DOCUMENT ME!
+     *
+     * @throws  Exception  DOCUMENT ME!
+     */
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        final Properties p = new Properties();
+        p.put("log4j.appender.Remote", "org.apache.log4j.net.SocketAppender");
+        p.put("log4j.appender.Remote.remoteHost", "localhost");
+        p.put("log4j.appender.Remote.port", "4445");
+        p.put("log4j.appender.Remote.locationInfo", "true");
+        p.put("log4j.rootLogger", "ALL,Remote");
+        org.apache.log4j.PropertyConfigurator.configure(p);
+        
+        
+        PropertyResourceBundle bundle;
+        
+        try
+        {
+            bundle = new PropertyResourceBundle(RESTfulInterfaceConnector.class.getResourceAsStream("service.properties"));
+        } catch(Exception ex) {
+            LOGGER.error("could not find local properties file 'service.properties': " + ex.getMessage(), ex);
+            throw ex;
+        }
+        
+        assertNotNull(bundle.getString("host"));
+        assertNotNull(bundle.getString("username"));
+        assertNotNull(bundle.getString("domain"));
+        assertNotNull(bundle.getString("password"));
+        
+        HOST = bundle.getString("host");
+        BASIC_AUTH_STRING = "Basic "
+                        + new String(Base64.encode(bundle.getString("username") 
+                                + "@" + bundle.getString("domain") 
+                                + ":" + bundle.getString("password")));
+    }
+    
+    private WebResource.Builder createAuthorisationHeader(final WebResource webResource)
+            throws RemoteException {
+        final WebResource.Builder builder = webResource.header("Authorization", BASIC_AUTH_STRING);
+        return builder;
+    }
+    
+    private final CidsBean insertDefaultCidsBean(JsonNode defaultCidsBeanNode)
+            throws RemoteException {
+        
+        final CidsBeanInfo beanInfo 
+                = new CidsBeanInfo(defaultCidsBeanNode.get(CidsBeanInfo.JSON_CIDS_OBJECT_KEY_IDENTIFIER).textValue());
+        
+        
+
+        final MultivaluedMap queryParameters = new MultivaluedMapImpl();
+        queryParameters.add("requestResultingInstance", "true");
+        final WebResource webResource = this.createWebResource(ENTITIES_API)
+                    .path(beanInfo.getDomainKey() + "." + beanInfo.getClassKey())
+                    .queryParams(queryParameters);
+        WebResource.Builder builder = this.createAuthorisationHeader(webResource);
+        builder = this.createMediaTypeHeaders(builder);
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("insertMetaObject for class '" + beanInfo.getDomainKey() + "." 
+                    + beanInfo.getClassKey() + "):" + webResource.toString());
+        }
+        
+
+        try {
+            final JsonNode objectNode = builder.post(ObjectNode.class,defaultCidsBeanNode);
+            if ((objectNode == null) || (objectNode.size() == 0)) {
+                LOGGER.error("could not insert meta object for class '" + beanInfo.getDomainKey() + "." 
+                        + beanInfo.getClassKey() + "': newly inserted meta object could not be found");
+                return null;
+            }
+
+            final CidsBean cidsBean;
+            try {
+                cidsBean = CidsBean.createNewCidsBeanFromJSON(false, objectNode.toString());
+            } catch (Exception ex) {
+                final String message = "could not deserialize cids bean from object node for class '"
+                            + beanInfo.getClassKey()
+                            + "': "
+                            + ex.getMessage();
+                LOGGER.error(message, ex);
+                throw new RemoteException(message, ex);
+            }
+
+            if (cidsBean != null) {
+                return cidsBean;
+            } else {
+                LOGGER.error("could not insert meta object for class '" + beanInfo.getClassKey() + "@" +  beanInfo.getDomainKey() 
+                            + "': newly inserted meta object could not be found");
+                return null;
+            }
+        } catch (UniformInterfaceException ue) {
+            final ClientResponse.Status status = ue.getResponse().getClientResponseStatus();
+            final String message = "could not insert meta object for class  '"
+                        + beanInfo.getClassKey()
+                        + "@"
+                        + beanInfo.getDomainKey() 
+                        + "': "
+                        + status.getReasonPhrase();
+
+            LOGGER.error(message, ue);
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(ue.getResponse().getEntity(String.class));
+            }
+            throw new RemoteException(message, ue);
+        }
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @throws  Exception  DOCUMENT ME!
+     */
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+     /**
+     * DOCUMENT ME!
+     */
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * DOCUMENT ME!
+     */
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void doNothing() {
+        
+    }
+}
